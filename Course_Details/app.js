e421c095d5aaa19a974dff0e4ea0fb8b1f5128be
Ebu <zahid.rpbd@gmail.com>
2018-05-07 17:50:00 +0600

    deleted

--- Course_Details/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-const course_details_Routes = require('./api/routes/course_details');
-
-app.use('/course_details',course_details_Routes);
-
-module.exports = app;
