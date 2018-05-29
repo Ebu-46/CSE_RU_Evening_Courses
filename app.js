3035ca42f53ebf6dbef1d04b61fedcb4daa353c2
Rana53 <ranajahid.cseru@gmail.com>
2018-05-30 00:49:13 +0600

    end point link added in app.js file.

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const availableCoursesRoutes = require('./Resource/Available_Courses/availableCourses');
-
+const coursesDetailse = require('./Resource/Course_Details/coursesDetails');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use('/EveningCourses/AvailableCourses',availableCoursesRoutes);
 
+app.use('/EveningCourses/AvailableCourses',availableCoursesRoutes);
+app.use('/EveningCourses/CoursesDetailse');
 
 
 module.exports = app;
